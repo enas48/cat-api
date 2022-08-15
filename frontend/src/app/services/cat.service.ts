a3d5e7e51dd1c2ed656c2c87e35ff1ad504e2393
enas48 <enasm4829@gmail.com>
2022-08-15 09:08:59 +0200

    fix back prod url

--- frontend/src/app/services/cat.service.ts
+++ frontend/src/app/services/cat.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http'; 
 import { Observable } from 'rxjs';
-const prodUrl='https://cat-api-beta.vercel.app/';
-const url='http://localhost:3000/';
+const prodUrl='https://cat-api-beta.vercel.app';
+const url='http://localhost:3000';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,18 +16,18 @@ export class CatService {
     }),
   };
   getCats(limit:Number): Observable<any> {
-    return  this.http.get<any>(`${prodUrl}/getbreeds/${limit}`);
+    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getbreeds/${limit}`);
   }
   getCat(id:String): Observable<any> {
-    return  this.http.get<any>(`${prodUrl}/getbreed/${id}`);
+    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getbreed/${id}`);
   }
   getImage(id:String): Observable<any> {
-    return  this.http.get<any>(`${prodUrl}/getimage/${id}`);
+    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getimage/${id}`);
   }
   getcatImages(id:String): Observable<any> {
-    return  this.http.get<any>(`${prodUrl}/catimages/${id}`);
+    return  this.http.get<any>(`https://cat-api-beta.vercel.app/catimages/${id}`);
   }
   searchByCatName(name:String): Observable<any> {
-    return  this.http.get<any>(`${prodUrl}/catbyname/${name}`);
+    return  this.http.get<any>(`https://cat-api-beta.vercel.app/catbyname/${name}`);
   }
 }
