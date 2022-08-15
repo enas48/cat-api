41539705ccd952235c6c854ad1191eeb84c15fc4
enas48 <enasm4829@gmail.com>
2022-08-15 22:02:33 +0200

    add preloader

--- frontend/src/app/services/cat.service.ts
+++ frontend/src/app/services/cat.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http'; 
 import { Observable } from 'rxjs';
-const url='http://localhost:3000';
-// const url='https://cat-api-backend.vercel.app';
+// const url='http://localhost:3000';
+ const url='https://cat-api-backend.vercel.app';
 
 @Injectable({
   providedIn: 'root'
