7dc9e3b7cc79cdbd2ddf0cca3fd0c10226f3350d
enas48 <enasm4829@gmail.com>
2022-08-15 10:44:48 +0200

    deploy

--- frontend/src/app/services/cat.service.ts
+++ frontend/src/app/services/cat.service.ts
@@ -16,18 +16,18 @@ export class CatService {
     }),
   };
   getCats(limit:Number): Observable<any> {
-    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getbreeds/${limit}`);
+    return  this.http.get<any>(`${url}/getbreeds/${limit}`);
   }
   getCat(id:String): Observable<any> {
-    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getbreed/${id}`);
+    return  this.http.get<any>(`${url}/getbreed/${id}`);
   }
   getImage(id:String): Observable<any> {
-    return  this.http.get<any>(`https://cat-api-beta.vercel.app/getimage/${id}`);
+    return  this.http.get<any>(`${url}/getimage/${id}`);
   }
   getcatImages(id:String): Observable<any> {
-    return  this.http.get<any>(`https://cat-api-beta.vercel.app/catimages/${id}`);
+    return  this.http.get<any>(`${url}/catimages/${id}`);
   }
   searchByCatName(name:String): Observable<any> {
-    return  this.http.get<any>(`https://cat-api-beta.vercel.app/catbyname/${name}`);
+    return  this.http.get<any>(`${url}/catbyname/${name}`);
   }
 }
